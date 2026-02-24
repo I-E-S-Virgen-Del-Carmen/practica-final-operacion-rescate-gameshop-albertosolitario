a80459bf19ff5c0b88b254f599fdf509166ca5ee
aescmun <aescmun1904@g.educaand.es>
2026-02-24 11:16:56 +0100

    Fix: Corregido bucle de inventario y error de nulo en VIP

--- src/main/java/com/gameshop/GestorInventario.java
+++ src/main/java/com/gameshop/GestorInventario.java
@@ -2,11 +2,11 @@ package com.gameshop;
 
 public class GestorInventario {
 
-    public void p(String[] n, int[] c) {
+    public void procesarInventario(String[] n, int[] c) {
         int t = 0;
 
         // BUG LÓGICO: El '- 1' hace que el último elemento (Halo) se ignore por completo.
-        for (int i = 0; i < n.length - 1; i++) {
+        for (int i = 0; i < n.length; i++) {
             System.out.println("Revisando stock de: " + n[i]);
 
             // CODE SMELL: Magic Number (5)
